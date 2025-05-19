1af0f1ab5eb15eff76499d95c81ac86d72ca01df
Prince Muchogo <108458278+PrinceMuchogo@users.noreply.github.com>
2025-05-19 11:30:10 +0200

    update

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+import "jsvectormap/dist/css/jsvectormap.css";
+import "flatpickr/dist/flatpickr.min.css";
+import "@/css/satoshi.css";
+import "@/css/style.css";
+import React, { useEffect, useState } from "react";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  // const pathname = usePathname();
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  return (
+    <html lang="en">
+      <body suppressHydrationWarning={true}>{children}</body>
+    </html>
+  );
+}
